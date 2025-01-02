2fd2cf3456f16a237655e01b4f093b85b9eb702c
bishal292 <bs9107709@gmail.com>
2025-01-03 01:56:28 +0530

    working on sorting algorithms

---
+++ SortingTechniques/src/bubbleSort/BubbleSort.java
@@ -0,0 +1,41 @@
+package bubbleSort;
+
+public class BubbleSort {
+
+    /**
+     * Sorts the given array using the bubble sort algorithm.
+     * 
+     * Bubble sort is a simple sorting algorithm that repeatedly steps through the list,
+     * compares adjacent elements and swaps them if they are in the wrong order. The pass
+     * through the list is repeated until the list is sorted. The algorithm gets its name
+     * because smaller elements "bubble" to the top of the list.
+     * 
+     * Time Complexity: O(n^2) in the worst and average case, where n is the number of elements.
+     * Space Complexity: O(1) as it is an in-place sorting algorithm.
+     * 
+     * @param arr the array to be sorted
+     */
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean isSwapped = false;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    isSwapped = true;
+                    arr[j] += arr[j+1];
+                    arr[j+1] = arr[j] - arr[j+1];
+                    arr[j] -= arr[j+1];
+                }
+            }
+            if(!isSwapped)break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,20,20,30,10};
+        bubbleSort(arr);
+
+        for (int elements : arr) {
+            System.out.print(elements+", ");
+        }
+    }
+}
---
+++ SortingTechniques/src/cycleSort/CycleSort.java
@@ -0,0 +1,5 @@
+package cycleSort;
+
+public class CycleSort {
+    
+}
