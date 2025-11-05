3c676a3a1df37bc0cbf736485df47421eb6515e5
bishal292 <bs9107709@gmail.com>
2025-11-05 21:20:54 +0530

    Completed Implementaion of all types of LinkedList along with its explanation

---
+++ LinkedList/src/linkedListImplementation_And_Usecase/CustomSinglyLinkedList.java
@@ -0,0 +1,141 @@
+package linkedListImplementation_And_Usecase;
+
+public class CustomSinglyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    /// Default constructor
+    public CustomSinglyLinkedList() {
+        this.size = 0;
+    }
+
+    /// Constructor with one element
+    public CustomSinglyLinkedList(int elem) {
+        add(elem);
+    }
+
+    /// Add element at the end
+    public void add(int elem) {
+        Node newNode = new Node(elem);
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            tail.nextNode = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    ///  Inserting at a particular index.
+    public void insert(int index, int value) {
+        if (index < 0 || index > size) throw new Error("Index Out Of Bound");
+        if (index == size) {///  inserting at the last index
+            add(value);
+            return;
+        }
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        int idx = 0;
+        Node ptr = head;
+        while (idx != index - 1) { ///  iterates and reach to the Node just behind the target index.
+            ptr = ptr.nextNode;
+            idx++;
+        }
+        Node newNode = new Node(value);
+        newNode.nextNode = ptr.nextNode;
+        ptr.nextNode = newNode;
+        size++;
+    }
+
+    /// Add element at the beginning
+    public void addFirst(int elem) {
+        Node newNode = new Node(elem);
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            newNode.nextNode = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    /// Remove the first element
+    public void removeFirst() {
+        if (head == null)
+            throw new RuntimeException("Linked list is empty.");
+        head = head.nextNode;
+        if (head == null) tail = null; /// If list becomes empty
+        size--;
+    }
+
+    /// Remove the last element
+    public void removeLast() {
+        if (head == null)
+            throw new RuntimeException("Linked list is empty.");
+        if (head.nextNode == null) {
+            head = null;
+            tail = null;
+        } else {
+            Node ptr = head;
+            while (ptr.nextNode != tail) {
+                ptr = ptr.nextNode;
+            }
+            tail = ptr; /// Moved the tail pointer to one set backward but it's next is still pointing to the the next Node. So,
+            tail.nextNode = null;
+        }
+        size--;
+    }
+
+    /// Get first element
+    public int getFirst() {
+        if (head == null)
+            throw new RuntimeException("Linked list is empty.");
+        return head.value;
+    }
+
+    /// Get last element
+    public int getLast() {
+        if (tail == null)
+            throw new RuntimeException("Linked list is empty.");
+        return tail.value;
+    }
+
+    /// Get size of the list
+    public int size() {
+        return size;
+    }
+
+    /// Print the linked list as a string
+    @Override
+    public String toString() {
+//        if (size == 0) return "[]";
+        if (size == 0) return " EMPTY ";
+
+        StringBuilder sb = new StringBuilder();
+//        sb.append("[");
+        Node ptr = head;
+        while (ptr != null) {
+            sb.append(ptr.value).append(" -> ");
+//            if (ptr.nextNode != null) sb.append(", ");
+            ptr = ptr.nextNode;
+        }
+//        sb.append("]");
+        sb.append(" END ");
+        return sb.toString();
+    }
+
+    /// Private Node class
+    private static class Node {
+        private int value;
+        private Node nextNode;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
---
+++ LinkedList/src/linkedListImplementation_And_Usecase/Main.java
@@ -0,0 +1,86 @@
+package linkedListImplementation_And_Usecase;
+
+public class Main {
+    public static void main(String[] args) {
+///        ------------------------------- Singly Linked List -------------------------------------
+        CustomSinglyLinkedList sll = new CustomSinglyLinkedList();
+        System.out.println(sll);
+//        sll.removeLast(); /// Will throw error as linked list is empty now.
+//        sll.add(12);
+//        sll.add(21);
+//        sll.add(1);
+//        sll.add(11);
+//        sll.add(23);
+//        sll.add(28);
+//        System.out.println(sll);
+//        sll.removeLast();
+//        System.out.println(sll);
+//        sll.addFirst(28);
+//        System.out.println(sll);
+//        sll.removeFirst();
+//        System.out.println(sll);
+//        sll.insert(0, 1);
+//        sll.insert(1, 2);
+//        sll.insert(2, 3);
+//        sll.insert(3, 4);
+//        sll.insert(4, 5);
+//        sll.insert(sll.size(), sll.size() + 1);
+//        System.out.println(sll);
+
+
+
+
+///        -------------------------------Doubly Linked List -------------------------------------
+        CustomDoublyLinkedList ll = new CustomDoublyLinkedList();
+        System.out.println(ll);
+//        ll.removeLast();
+//        ll.add(12);
+//        ll.add(21);
+//        ll.add(1);
+//        ll.add(11);
+//        ll.add(23);
+//        ll.add(28);
+//        System.out.println(ll);
+//        ll.removeLast();
+//        System.out.println(ll);
+//        ll.addFirst(28);
+//        System.out.println(ll);
+//        ll.removeFirst();
+//        System.out.println(ll);
+//        ll.insert(0, 1);
+//        ll.insert(1, 2);
+//        ll.insert(2, 3);
+//        ll.insert(3, 4);
+//        ll.insert(4, 5);
+//        ll.insert(ll.size(), ll.size() + 1);
+//        System.out.println(ll);
+//        ll.displayInreverse(); /// Only can be used with Doubly linked list as both way traversal is easy due to prev Pointer.
+
+///        ------------------------------- Circular Linked List -------------------------------------
+
+        CircularLinkedList cll = new CircularLinkedList();
+        System.out.println(cll);
+//        cll.removeLast();
+        cll.add(12);
+        cll.add(21);
+        cll.add(1);
+        cll.add(11);
+        cll.add(23);
+        cll.add(28);
+        System.out.println(cll);
+        cll.removeLast();
+        System.out.println(cll);
+        cll.addFirst(28);
+        System.out.println(cll);
+        cll.removeFirst();
+        System.out.println(cll);
+        cll.insert(0, 1);
+        cll.insert(1, 2);
+        cll.insert(2, 3);
+        cll.insert(3, 4);
+        cll.insert(4, 5);
+        cll.insert(cll.size(), cll.size() + 1);
+        System.out.println(cll);
+    }
+
+}
